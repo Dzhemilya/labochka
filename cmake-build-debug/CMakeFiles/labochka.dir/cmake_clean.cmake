6d6981e8806fefdd2c483f9633863cb50bed3aae
MaximInnopolis <71584246+MaximInnopolis@users.noreply.github.com>
2021-02-09 19:22:56 +0300

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/labochka.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/labochka.dir/main.cpp.obj"
+  "labochka.exe"
+  "labochka.exe.manifest"
+  "labochka.pdb"
+  "liblabochka.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/labochka.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/labochka.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for labochka.
+# This may be replaced when dependencies are built.
